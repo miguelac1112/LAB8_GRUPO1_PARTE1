d7a2d58d2020028d17ba1686cba4c9a07abaa5a6
Miguel <a20190050@pucp.edu.pe>
2022-11-08 20:52:25 -0500

    DSADASDSA

---
+++ src/main/java/com/example/lab8_parte1/Lab8Parte1Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_parte1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab8Parte1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab8Parte1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab8_parte1/Lab8Parte1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_parte1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8Parte1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
