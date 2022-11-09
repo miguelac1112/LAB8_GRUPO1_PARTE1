c6d100f1f56b41bd554254f4262cbb8a18caa956
caleb <caleb.casapaico@pucp.edu.pe>
2022-11-08 21:06:32 -0500

    controller corregido

--- src/main/java/com/example/lab8_parte1/Controller/PeliculasController.java
+++ src/main/java/com/example/lab8_parte1/Controller/PeliculasController.java
@@ -1,21 +1,23 @@
 package com.example.lab8_parte1.Controller;
 
 
+import com.example.lab8_parte1.Entity.Pelicula;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(value = "/peliculas")
 public class PeliculasController {
     @Autowired
-    SupplierRepository supplierRepository;
-    @GetMapping("/supplier")
-    public List<Supplier> supplierList(){
-        return supplierRepository.findAll();
+    PeliculasController peliculasController;
+    @GetMapping("/lista")
+    public List<Pelicula> supplierList(){
+        return peliculasController.findAll();
     }
 }
